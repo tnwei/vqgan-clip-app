99342843ae58ed384273aa2dcd3607ae36c13d50
Tan Nian Wei <tannianwei@aggienetwork.com>
2021-07-17 22:24:58 +0800

    refactor: separated logic into utils

--- gradio-app.py
+++ gradio-app.py
@@ -1,17 +1,13 @@
 import gradio as gr
 import argparse
 import math
-import io
 from pathlib import Path
 import sys
 
 sys.path.append("./taming-transformers")
 
-from IPython import display
-from omegaconf import OmegaConf
 from PIL import Image
 import requests
-from taming.models import cond_transformer, vqgan
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
@@ -21,182 +17,16 @@ from tqdm import tqdm
 
 from CLIP import clip
 
-
-def sinc(x):
-    return torch.where(x != 0, torch.sin(math.pi * x) / (math.pi * x), x.new_ones([]))
-
-
-def lanczos(x, a):
-    cond = torch.logical_and(-a < x, x < a)
-    out = torch.where(cond, sinc(x) * sinc(x / a), x.new_zeros([]))
-    return out / out.sum()
-
-
-def ramp(ratio, width):
-    n = math.ceil(width / ratio + 1)
-    out = torch.empty([n])
-    cur = 0
-    for i in range(out.shape[0]):
-        out[i] = cur
-        cur += ratio
-    return torch.cat([-out[1:].flip([0]), out])[1:-1]
-
-
-def resample(input, size, align_corners=True):
-    n, c, h, w = input.shape
-    dh, dw = size
-
-    input = input.view([n * c, 1, h, w])
-
-    if dh < h:
-        kernel_h = lanczos(ramp(dh / h, 2), 2).to(input.device, input.dtype)
-        pad_h = (kernel_h.shape[0] - 1) // 2
-        input = F.pad(input, (0, 0, pad_h, pad_h), "reflect")
-        input = F.conv2d(input, kernel_h[None, None, :, None])
-
-    if dw < w:
-        kernel_w = lanczos(ramp(dw / w, 2), 2).to(input.device, input.dtype)
-        pad_w = (kernel_w.shape[0] - 1) // 2
-        input = F.pad(input, (pad_w, pad_w, 0, 0), "reflect")
-        input = F.conv2d(input, kernel_w[None, None, None, :])
-
-    input = input.view([n, c, h, w])
-    return F.interpolate(input, size, mode="bicubic", align_corners=align_corners)
-
-
-class ReplaceGrad(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x_forward, x_backward):
-        ctx.shape = x_backward.shape
-        return x_forward
-
-    @staticmethod
-    def backward(ctx, grad_in):
-        return None, grad_in.sum_to_size(ctx.shape)
-
-
-replace_grad = ReplaceGrad.apply
-
-
-class ClampWithGrad(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, input, min, max):
-        ctx.min = min
-        ctx.max = max
-        ctx.save_for_backward(input)
-        return input.clamp(min, max)
-
-    @staticmethod
-    def backward(ctx, grad_in):
-        input, = ctx.saved_tensors
-        return (
-            grad_in * (grad_in * (input - input.clamp(ctx.min, ctx.max)) >= 0),
-            None,
-            None,
-        )
-
-
-clamp_with_grad = ClampWithGrad.apply
-
-
-def vector_quantize(x, codebook):
-    d = (
-        x.pow(2).sum(dim=-1, keepdim=True)
-        + codebook.pow(2).sum(dim=1)
-        - 2 * x @ codebook.T
-    )
-    indices = d.argmin(-1)
-    x_q = F.one_hot(indices, codebook.shape[0]).to(d.dtype) @ codebook
-    return replace_grad(x_q, x)
-
-
-class Prompt(nn.Module):
-    def __init__(self, embed, weight=1.0, stop=float("-inf")):
-        super().__init__()
-        self.register_buffer("embed", embed)
-        self.register_buffer("weight", torch.as_tensor(weight))
-        self.register_buffer("stop", torch.as_tensor(stop))
-
-    def forward(self, input):
-        input_normed = F.normalize(input.unsqueeze(1), dim=2)
-        embed_normed = F.normalize(self.embed.unsqueeze(0), dim=2)
-        dists = input_normed.sub(embed_normed).norm(dim=2).div(2).arcsin().pow(2).mul(2)
-        dists = dists * self.weight.sign()
-        return (
-            self.weight.abs()
-            * replace_grad(dists, torch.maximum(dists, self.stop)).mean()
-        )
-
-
-def fetch(url_or_path):
-    if str(url_or_path).startswith("http://") or str(url_or_path).startswith(
-        "https://"
-    ):
-        r = requests.get(url_or_path)
-        r.raise_for_status()
-        fd = io.BytesIO()
-        fd.write(r.content)
-        fd.seek(0)
-        return fd
-    return open(url_or_path, "rb")
-
-
-def parse_prompt(prompt):
-    if prompt.startswith("http://") or prompt.startswith("https://"):
-        vals = prompt.rsplit(":", 3)
-        vals = [vals[0] + ":" + vals[1], *vals[2:]]
-    else:
-        vals = prompt.rsplit(":", 2)
-    vals = vals + ["", "1", "-inf"][len(vals) :]
-    return vals[0], float(vals[1]), float(vals[2])
-
-
-class MakeCutouts(nn.Module):
-    def __init__(self, cut_size, cutn, cut_pow=1.0):
-        super().__init__()
-        self.cut_size = cut_size
-        self.cutn = cutn
-        self.cut_pow = cut_pow
-
-    def forward(self, input):
-        sideY, sideX = input.shape[2:4]
-        max_size = min(sideX, sideY)
-        min_size = min(sideX, sideY, self.cut_size)
-        cutouts = []
-        for _ in range(self.cutn):
-            size = int(
-                torch.rand([]) ** self.cut_pow * (max_size - min_size) + min_size
-            )
-            offsetx = torch.randint(0, sideX - size + 1, ())
-            offsety = torch.randint(0, sideY - size + 1, ())
-            cutout = input[:, :, offsety : offsety + size, offsetx : offsetx + size]
-            cutouts.append(resample(cutout, (self.cut_size, self.cut_size)))
-        return clamp_with_grad(torch.cat(cutouts, dim=0), 0, 1)
-
-
-def load_vqgan_model(config_path, checkpoint_path):
-    config = OmegaConf.load(config_path)
-    if config.model.target == "taming.models.vqgan.VQModel":
-        model = vqgan.VQModel(**config.model.params)
-        model.eval().requires_grad_(False)
-        model.init_from_ckpt(checkpoint_path)
-    elif config.model.target == "taming.models.cond_transformer.Net2NetTransformer":
-        parent_model = cond_transformer.Net2NetTransformer(**config.model.params)
-        parent_model.eval().requires_grad_(False)
-        parent_model.init_from_ckpt(checkpoint_path)
-        model = parent_model.first_stage_model
-    else:
-        raise ValueError(f"unknown model type: {config.model.target}")
-    del model.loss
-    return model
-
-
-def resize_image(image, out_size):
-    ratio = image.size[0] / image.size[1]
-    area = min(image.size[0] * image.size[1], out_size[0] * out_size[1])
-    size = round((area * ratio) ** 0.5), round((area / ratio) ** 0.5)
-    return image.resize(size, Image.LANCZOS)
-
+from utils import (
+    load_vqgan_model,
+    MakeCutouts,
+    parse_prompt,
+    resize_image,
+    Prompt,
+    fetch,
+    synth,
+    checkin,
+)
 
 # Hacky method to preserve state
 class State:
@@ -241,6 +71,7 @@ def run(
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Using device:", device)
 
+    # Load models
     if continue_prev_run is True:
         model = state.model
         perceptor = state.perceptor
@@ -255,6 +86,7 @@ def run(
             .to(device)
         )
 
+    # Initialize models
     cut_size = perceptor.visual.input_resolution
     e_dim = model.quantize.e_dim
     f = 2 ** (model.decoder.num_resolutions - 1)
@@ -289,6 +121,7 @@ def run(
         std=[0.26862954, 0.26130258, 0.27577711],
     )
 
+    ## Initialize prompts
     pMs = []
 
     for prompt in args.prompts:
@@ -308,23 +141,8 @@ def run(
         embed = torch.empty([1, perceptor.visual.output_dim]).normal_(generator=gen)
         pMs.append(Prompt(embed, weight).to(device))
 
-    def synth(z):
-        z_q = vector_quantize(z.movedim(1, 3), model.quantize.embedding.weight).movedim(
-            3, 1
-        )
-        return clamp_with_grad(model.decode(z_q).add(1).div(2), 0, 1)
-
-    @torch.no_grad()
-    def checkin(i, losses):
-        losses_str = ", ".join(f"{loss.item():g}" for loss in losses)
-        tqdm.write(f"i: {i}, loss: {sum(losses).item():g}, losses: {losses_str}")
-        out = synth(z)
-        im = TF.to_pil_image(out[0].cpu())
-        return im
-        # display.display(display.Image('progress.png')) # ipynb only
-
     def ascend_txt():
-        out = synth(z)
+        out = synth(model, z)
         iii = perceptor.encode_image(normalize(make_cutouts(out))).float()
 
         result = []
@@ -340,7 +158,7 @@ def run(
     for i in range(num_steps):
         opt.zero_grad()
         lossAll = ascend_txt()
-        im = checkin(i, lossAll)
+        im = checkin(i, lossAll, model, z)
         loss = sum(lossAll)
         loss.backward()
         opt.step()
@@ -350,9 +168,6 @@ def run(
     state.prev_im = im
     return im
 
-
-# TODO: Allow visualizing intermediate steps
-# Gradio doesn't seem very useful given that I want state to implement callbacks for incremental updating images
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -373,7 +188,7 @@ iface = gr.Interface(
                 "wikiart_1024",
             ]
         ),
-        gr.inputs.Slider(minimum=20, maximum=2000, step=25, default=300),
+        gr.inputs.Slider(minimum=20, maximum=2000, step=20, default=300),
         gr.inputs.Number(default=300, label="Xdim"),
         gr.inputs.Number(default=300, label="Ydim"),
         gr.inputs.Checkbox(label="Continue previous run"),
